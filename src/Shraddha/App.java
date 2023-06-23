02d7e534b64a7f305c7b9d74b248614ee711af24
shradhachuri <shradhachuri@gmail.com>
2023-06-23 11:13:22 +0000

    Changes done

--- src/Shraddha/App.java
+++ src/Shraddha/App.java
@@ -8,7 +8,7 @@ public static void main(String [] args) {
 		System.out.println(10*(20+20));
 		System.out.println(20/20);
 		System.out.println(20/20);
-		System.out.println(10%20);
+	
 	
 		
 		System.out.println();
