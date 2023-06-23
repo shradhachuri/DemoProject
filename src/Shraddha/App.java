eef2f3ac66592502452a8cee70704677a314b5aa
shradhachuri <shradhachuri@gmail.com>
2023-06-23 11:01:03 +0000

    Update Code

--- src/Shraddha/App.java
+++ src/Shraddha/App.java
@@ -1,11 +1,19 @@
 package Shraddha;
 
 public class App {
+	
 public static void main(String [] args) {
 		
-		System.out.println("Hello World!");
+		System.out.println((10*20)+10);
+		System.out.println(10*(20+20));
+		System.out.println(20/20);
+		System.out.println(20/20);
+		System.out.println(10%20);
 	
 		
-	}
-
+		System.out.println();
+		
 }
+}
+
+
